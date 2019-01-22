855eb30c4d56db993ed36a89c3f078374814f161
Karthikeyan Kailasam <karthikeyankailasam@discover.com>
2019-01-21 20:44:35 -0600

    simplified bitbar runner shell script

--- run-tests.sh
+++ run-tests.sh
@@ -1,79 +1,61 @@
 #!/bin/bash
 
-##### Cloud testrun dependencies start
-echo "[ssa-client] Extracting tests.zip..."
-unzip bitbartest.zip
+Echo() {
+  TS=`date +%Y%m%d-%H:%M:%S`
+  echo "### [$TS] $@"
+}
 
-echo "[ssa-client] NOT Starting Appium, start it in your test script if needed!"
+Echo "Starting test"
 
-##### Cloud testrun dependencies end.
-export APPIUM_APPFILE=$PWD/calculator.ipa #dummy app, not the actual app
+Echo "Current folder: `pwd`"
+Echo "Current folder files [Before Run]:"
+ls -ltra
 
-## Run the test:
-echo "[ssa-client] Running test ${TEST}"
-
-export APPFILE=$PWD/ios/build/Build/Products/Debug-iphonesimulator/TestApp.app
-
-# put local tools to path
-export PATH=$PATH:$PWD
-
+### cleanup screenshots folder
 export SCREENSHOTSFOLDER=$PWD/screenshots
-
-#################################
-
-echo "using APPFILE: ${APPFILE}"
-
-cd "$(dirname "$0")" || exit
-
-
-echo "Before install $(date)"
-
-if [ -z ${UDID} ] ; then
-	export UDID=${IOS_UDID}
-fi
-	echo "UDID is ${UDID}"
-
-#xcrun instruments -s devices | grep -v 'instruments' | grep -v 'Simulator' | grep 65b82a5578d9ff66fa41e200ee92a92112b87225 | tail -1
-
 rm -rf "$SCREENSHOTSFOLDER"
-rm ./*.xml
 mkdir -p "$SCREENSHOTSFOLDER"
 
-echo $SCREENSHOTSFOLDER
+### device udid
+if [ -z ${UDID} ] ; then
+  export UDID=${IOS_UDID}
+fi
+echo "UDID is ${UDID}"
 
+### install applesimutils
+Echo "Starting install of applesimutils"
 brew tap wix/brew
 brew install applesimutils
+Echo "Completed install of applesimutils"
 
-node --version
-npm -version
-watchman --version
+### version checks
+Echo "node version: `node --version`"
+Echo "npm  version: `npm -version`"
+Echo "watchman version: `watchman --version`"
 
-echo "Npm install"
+### install npm packages
+Echo "Starting npm install"
 npm install
+Echo "Completed npm install"
 
-#build project
-#"${PWD}/node_modules/.bin/detox" build --configuration ios.sim.release
+### add local npm bin to path
+export PATH=$PATH:$PWDi/node_modules/.bin/
 
-#echo "Launching Detox server"
-# no need to start server for iOS simulator
-#"${PWD}/node_modules/.bin/detox" -version
-#"${PWD}/node_modules/.bin/detox" run-server &
-#"${PWD}/node_modules/.bin/detox" run-server > detox-server.log 2>&1 &
-
-cat package.json
-
-"${PWD}/node_modules/.bin/detox" test --configuration ios.sim.debug --loglevel verbose > detox.log 2>&1
+### start detox test
+Echo "Starting detox test"
+detox test --configuration ios.sim.debug --loglevel verbose > detox.log 2>&1
 scriptExitStatus=$?
+Echo "Completed detox test, exit status: '${scriptExitStatus}'"
 
-ls -la detox.log
+### export logs
+Echo "Current folder [`pwd`] contents [Post Run]"
+ls -ltra
 
-echo "detox-test-log"
+Echo "detox-test-log"
 cat detox.log
 
-echo "detox-server-log"
+Echo "detox-server-log"
 cat detox-server.log
-
-echo "Test has been run $(date), exit status: '${scriptExitStatus}'"
 
 mv ./*.xml TEST-all.xml
 
